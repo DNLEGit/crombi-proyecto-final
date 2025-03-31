e2c5c22309134ad3f19330462e3b067a42823815
Danilo Nicolás Lorefice <dnlegithub@gmail.com>
2025-03-31 18:18:05 -0300

    Add Prisma setup, user and role models, and seed script

--- proyecto-final/app/(pages)/user/page.tsx
+++ proyecto-final/app/(pages)/user/page.tsx
@@ -1,7 +1,28 @@
 export default function UserPage() {
     return (
         <div>
-            <h1>User Page</h1>
+            <div>
+                <h1>User Page</h1>
+            </div>
+
+            <div>
+                <form action="sing-up">
+                    <div>
+                        <p>Create a new user</p>
+                        <label htmlFor="username">Username:</label>
+                        <input type="text" id="username" name="username" required />
+
+                        <label htmlFor="email">Email:</label>
+                        <input type="email" id="email" name="email" required />
+
+                        <label htmlFor="password">Password:</label>
+                        <input type="password" id="password" name="password" required />
+
+                        <button type="submit">Sign Up</button>
+                    </div>
+
+                </form>
+            </div>
         </div>
     );
 }
