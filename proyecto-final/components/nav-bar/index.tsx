f2e2117a40d89384ad34498f7b2f79616a7b9d21
Nicolas <dnlegithub@gmail.com>
2025-04-10 11:32:50 -0300

    fix: Improve user role handling in NavBar and clean up code formatting

--- proyecto-final/components/nav-bar/index.tsx
+++ proyecto-final/components/nav-bar/index.tsx
@@ -14,12 +14,11 @@ const NavBar: React.FC = () => {
     const pathname = usePathname();
 
     useEffect(() => {
-        async function checkAuth(){
+        async function checkAuth() {
             const res = await fetch("/api/auth/user", { cache: "no-store" });
             const data = await res.json();
             setIsAuthenticated(data.isAuthenticated)
-            setRole(data.user.role)
-            console.log(data.user.role)
+            setRole(data.user?.role || "")
         }
         checkAuth();
     }, [pathname])
@@ -31,8 +30,8 @@ const NavBar: React.FC = () => {
             router.push("/");
         }, 1000);
     };
-    
-        return (
+
+    return (
         <div className='flex justify-between items-center p-4 bg-gradient-to-tr from-purple-600 via-black to-purple-900 h-26 text-white '>
 
             {/* Botón Home a la izquierda */}
@@ -88,40 +87,40 @@ const NavBar: React.FC = () => {
                             <div className='m-2 hover:text-[#8858ed] text-[16px]'>
                                 <Link href={'/admin'}>Admin Panel</Link>
                             </div>
-                           
+
                         </>
                     ) : null}
 
-                {isAuthenticated ? (
-                   
-                    <>
+                    {isAuthenticated ? (
 
-                        <div className='m-2 hover:text-[#8858ed] text-[16px]'>
-                            <Link href={'/user'}>User</Link>
-                        </div>
-                        <div>
-                        <div className='m-2 hover:text-[#8858ed]'>
-                                <button onClick={handleLogout}>
-                                    Logout
-                                </button>
+                        <>
+
+                            <div className='m-2 hover:text-[#8858ed] text-[16px]'>
+                                <Link href={'/user'}>User</Link>
                             </div>
-                        </div>
-                
-                        <div className='flex justify-center items-center max-w-full'>
-                            {/* Icono del carrito */}
-                            <div className='m-2 hover:text-[#8858ed]'>
-                                <Link href={'/cart'}>
-                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
-                                        <path d="M2.25 2.25a.75.75 0 0 0 0 1.5h1.386c.17 0 .318.114.362.278l2.558 9.592a3.752 3.752 0 0 0-2.806 3.63c0 .414.336.75.75.75h15.75a.75.75 0 0 0 0-1.5H5.378A2.25 2.25 0 0 1 7.5 15h11.218a.75.75 0 0 0 .674-.421 60.358 60.358 0 0 0 2.96-7.228.75.75 0 0 0-.525-.965A60.864 60.864 0 0 0 5.68 4.509l-.232-.867A1.875 1.875 0 0 0 3.636 2.25H2.25ZM3.75 20.25a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0ZM16.5 20.25a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0Z" />
-                                    </svg>
-                                </Link>
+                            <div>
+                                <div className='m-2 hover:text-[#8858ed]'>
+                                    <button onClick={handleLogout}>
+                                        Logout
+                                    </button>
+                                </div>
                             </div>
-                        </div>
-                    </>
-                ) : (
-                    <div className='m-2 hover:text-[#8858ed] text-[16px]'><Link href="/login">Login</Link></div>
-                    
-                )}
+
+                            <div className='flex justify-center items-center max-w-full'>
+                                {/* Icono del carrito */}
+                                <div className='m-2 hover:text-[#8858ed]'>
+                                    <Link href={'/cart'}>
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
+                                            <path d="M2.25 2.25a.75.75 0 0 0 0 1.5h1.386c.17 0 .318.114.362.278l2.558 9.592a3.752 3.752 0 0 0-2.806 3.63c0 .414.336.75.75.75h15.75a.75.75 0 0 0 0-1.5H5.378A2.25 2.25 0 0 1 7.5 15h11.218a.75.75 0 0 0 .674-.421 60.358 60.358 0 0 0 2.96-7.228.75.75 0 0 0-.525-.965A60.864 60.864 0 0 0 5.68 4.509l-.232-.867A1.875 1.875 0 0 0 3.636 2.25H2.25ZM3.75 20.25a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0ZM16.5 20.25a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0Z" />
+                                        </svg>
+                                    </Link>
+                                </div>
+                            </div>
+                        </>
+                    ) : (
+                        <div className='m-2 hover:text-[#8858ed] text-[16px]'><Link href="/login">Login</Link></div>
+
+                    )}
 
                 </div>
 
